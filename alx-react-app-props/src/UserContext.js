68b7daeee8530f9ba989b8bde8af190b247a89c0
Brian <mungaibrian011@gmail.com>
2025-09-06 08:17:24 +0300

    Refactor Prop Drilling to Use Context API

---
+++ alx-react-app-props/src/UserContext.js
@@ -0,0 +1,16 @@
+
+
+
+import React, { createContext } from 'react';
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children, value }) => {
+  return (
+    <UserContext.Provider value={value}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContext;
